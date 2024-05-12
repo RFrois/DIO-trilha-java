ad5e1d028af5264e4897c2c063778c4e10ccdd23
Rafael <rafael.frois1@gmail.com>
2024-05-12 00:46:45 -0300

    Operadores lógicos

---
+++ JAVA-BASICO/src/edu/dioSantander/operadores/operadoresLogicos.java
@@ -0,0 +1,26 @@
+package edu.dioSantander.operadores;
+
+public class operadoresLogicos {
+
+    public static void main(String[] args) {
+        
+        boolean condicao1 = true;
+        boolean condicao2 = false;
+
+        if (condicao2 == false && 3<5){
+
+            System.out.println("Duas condições verdadeiras.");
+        }
+
+        if (condicao1 || condicao2) {
+
+            System.out.println("Uma ou outra");
+            
+        }
+
+        
+
+
+
+    }
+}
