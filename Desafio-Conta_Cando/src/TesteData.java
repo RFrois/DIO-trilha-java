5312ba47a0caf51bb490f77cbc716ea27c0cf59a
Rafael <rafael.frois1@gmail.com>
2024-06-05 00:20:25 -0300

    IA

---
+++ Desafio-Conta_Cando/src/TesteData.java
@@ -0,0 +1,28 @@
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+
+public class TesteData {
+
+    public static void main(String[] args) {
+        
+        
+        SimpleDateFormat inicial = new SimpleDateFormat("yyyy/MM/dd");
+        SimpleDateFormat final1 = new SimpleDateFormat("dd/MM");
+       
+        Date dataInicial = new Date();
+
+        
+
+
+        System.out.println(inicial.format(dataInicial));
+        System.out.println(final1.format(dataInicial));
+
+        String data01 = String.parse
+
+        
+
+
+    }
+
+}
