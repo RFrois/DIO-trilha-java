bd2488e5d24d1415ba0a8e63ace7c692739e629f
Rafael <rafael.frois1@gmail.com>
2025-02-07 10:25:54 -0300

    Spring boot primeiros passos

---
+++ primeiros_passos/src/main/java/com/br/dio/primeiros_passos/Calculadora.java
@@ -0,0 +1,11 @@
+package com.br.dio.primeiros_passos;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Calculadora {
+
+    public int somar(int soma1, int soma2){
+        return soma1  +soma2;
+    }
+}
---
+++ primeiros_passos/src/main/java/com/br/dio/primeiros_passos/PrimeirosPassosApplication.java
@@ -0,0 +1,22 @@
+package com.br.dio.primeiros_passos;
+
+import ch.qos.logback.core.encoder.JsonEscapeUtil;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeirosPassosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeirosPassosApplication.class, args);
+
+		
+
+
+	}
+
+
+
+
+
+}
